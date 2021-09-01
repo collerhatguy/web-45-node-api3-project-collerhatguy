805d0cf3772129964613ceaf027041b8d7eafec8
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-09-01 11:09:59 -0400

    add middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,35 @@
+const { getById } = require("../users/users-model")
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const { method, url, timestamp } = req
+  console.log(method, url, timestamp)
+  next()
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  try {
+    const { id } = req.params
+    const user = await getById(id)
+    user ? req.user = user : res.status(404).json({ message: "user not found" })
+    next()
+  } catch(err) {
+    res.status(500).json({ message: err.message })
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  const { body } = req
+  body.name ? next() : res.status(400).json({ message: "missing required name field" })
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  const { body } = req
+  body.text ? next() : res.status(400).json({ message: "missing required text field" })
 }
 
 // do not forget to expose these functions to other modules
+module.exports = { logger, validateUserId, validateUser, validatePost }
